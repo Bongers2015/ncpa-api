b5303d6dd84f3a954190181a65f080d09c5f64ae
rudivandrunen-spronq <rudi.vandrunen@spronq.com>
2019-12-04 10:50:31 +0100

    devops additions

---
+++ certs/certs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+openssl genrsa -des3 -out rootCA.key 2048
+openssl req -x509 -new -nodes -key rootCA.key -sha256 -days 1024 -out rootCA.pem
+## // enter domain for common name (localhost)
+openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config <( cat server.cnf )
+openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256 -extfile v3.ext
